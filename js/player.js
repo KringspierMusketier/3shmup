70d6ed9617867261863058dcb59fd1289c1d426a
KringspierMusketier <moraca.filip@gmail.com>
2017-10-11 12:47:56 +0200

    I see dead objects

--- js/player.js
+++ js/player.js
@@ -1,20 +1,21 @@
 class Player {
     constructor(posX, posZ) {
-        this.ship = new THREE.Mesh();
-        this.ship.position.set(posX, 0, posZ);
-        this.loader = new THREE.JSONLoader();
-        this.loader.load('models/playerShip.json', function(geometry) {
-            this.ship = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-                color: new THREE.Color(0x000000),
+        var loader = new THREE.JSONLoader();
+        loader.load('models/playerShip_v1.1.json', function(object) {
+            var ship = new THREE.Mesh(object, new THREE.MeshBasicMaterial({
+                color: new THREE.Color(0x00ff00),
                 wireframe: true
             }))
+            ship.position.set(posX, 0, posZ);
+            scene.add(ship);
         });
 
-        this.direction = new THREE.Vector3();
-        this.direction.set(0,0,0);
-        this.direction.normalize();
+        /**this.geometry = new THREE.CubeGeometry(1,1,1);
+        this.material = new THREE.MeshBasicMaterial({color: 0xff0000});
+        this.testMesh = new THREE.Mesh(this.geometry, this.material);
+        this.testMesh.position.set(posX, 5, posZ);
+        scene.add(this.testMesh);**/
 
         this.state = 'dead';
-        scene.add(this.ship);
     }
 }
