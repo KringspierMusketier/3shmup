895e762392881d1f298c0e3a8245eef5eba1372f
KringspierMusketier <moraca.filip@gmail.com>
2017-10-13 13:49:00 +0200

    movement speed tied to variable

--- js/player.js
+++ js/player.js
@@ -1,21 +1,22 @@
 class Player {
     constructor(posX, posZ) {
         this.ship = models[0];
+        this.speed = 1.0;
         scene.add(this.ship);
     }
 
     moveLeft() {
-        this.ship.position.x -= 1.0;
+        this.ship.position.x -= this.speed;
     }
 
     moveRight() {
-        this.ship.position.x += 1.0;
+        this.ship.position.x += this.speed;
     }
     moveUp() {
-        this.ship.position.z -= 1.0;
+        this.ship.position.z -= this.speed;
     }
     moveDown() {
-        this.ship.position.z += 1.0;
+        this.ship.position.z += this.speed;
     }
     shoot() {
         var bullet = new PlayerBullet(this.ship.positionx, this.ship.position.z);
