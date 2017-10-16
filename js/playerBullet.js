8f1f7b240d831506ff717631ad94d8d6c1811ded
Emiel de Wit <wit1605@student.nhl.nl>
2017-10-16 13:29:50 +0200

    bulletsWork

--- js/playerBullet.js
+++ js/playerBullet.js
@@ -19,6 +19,7 @@ class PlayerBullet{
 
     constructor(posX, posZ) {
 
+        this.shipPos = posZ;
         this.direction = new THREE.Vector3;
         this.direction.set(0, 0, -1);
         this.clock = new THREE.Clock();
@@ -26,8 +27,17 @@ class PlayerBullet{
         //var geo = new THREE.ExtrudeGeometry(bShape, extrudeSettings);
         var geo = new THREE.CubeGeometry(1, 1, 1);
         var mat = new THREE.MeshBasicMaterial(0x551A8B);
-        this.pBullet = new THREE.Mesh(geo, mat);
-        this.pBullet.position.set(posX, 0, posZ);
+        this.pBullet = new THREE.Group();
+        this.mpBullet = new THREE.Mesh(geo, mat);
+        this.lpBullet = new THREE.Mesh(geo, mat);
+        this.rpBullet = new THREE.Mesh(geo, mat);
+        this.lpBullet.position.set(posX - 2.5, 0, posZ+1);
+        this.mpBullet.position.set(posX, 0, posZ);
+        this.rpBullet.position.set(posX + 2.5, 0, posZ+1);
+        this.pBullet.add(this.lpBullet);
+        this.pBullet.add(this.rpBullet);
+        this.pBullet.add(this.mpBullet);
+
         scene.add(this.pBullet);
         playerBulletList.push(this);
         //this.checkWorldBounds = true;
@@ -36,6 +46,10 @@ class PlayerBullet{
 
     movement() {
         this.pBullet.position.z -= this.speed;
+        if (this.pBullet.position.z < (-45 - this.shipPos)) {
+            scene.remove(this.pBullet);
+            playerBulletList[this] = null;
+        }
     }
 }
 
