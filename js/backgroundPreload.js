a6d81d6cbe1ace5bf5ea564bd51a4d8704bf96c0
Emiel de Wit <wit1605@student.nhl.nl>
2017-10-27 12:59:24 +0200

    background fixed

--- js/backgroundPreload.js
+++ js/backgroundPreload.js
@@ -4,46 +4,46 @@ class BackgroundPreload {
     constructor() {
         var loader = new THREE.JSONLoader(manager);
 
-        loader.load('models/bg/BGisland.json', function (geometry) {
-            var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
-                color: new THREE.Color(0xDDDD22)
-            }))
-            bgchunk.push(mesh.clone());
-        });
+        //loader.load('models/bg/BGisland.json', function (geometry) {
+        //    var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
+        //        color: new THREE.Color(0xDDDD22)
+        //    }))
+        //    bgchunk.push(mesh.clone());
+        //});
 
         loader.load('models/bg/BGbeach.json', function (geometry) {
             var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
                 color: new THREE.Color(0xDDDD22)
             }))
-            bgchunk.push(mesh.clone());
+            bgchunk[2] = mesh.clone();
         });
 
         loader.load('models/bg/BGforrest.json', function (geometry) {
             var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
                 color: new THREE.Color(0x22DD22)
             }))
-            bgchunk.push(mesh.clone());
+            bgchunk[5] = mesh.clone();
         });
 
         loader.load('models/bg/BGcity.json', function (geometry) {
             var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
                 color: new THREE.Color(0xDDDDDD)
             }))
-            bgchunk.push(mesh.clone());
+            bgchunk[0] = mesh.clone();
         });
 
         loader.load('models/bg/BGhole.json', function (geometry) {
             var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
                 color: new THREE.Color(0x654321)
             }))
-            bgchunk.push(mesh.clone());
+            bgchunk[3] = mesh.clone();
         });
 
         loader.load('models/bg/loopbackground.json', function (geometry) {
             var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
                 color: new THREE.Color(0x653311)
             }))
-            bgchunk.push(mesh.clone());
+            bgchunk[4] = mesh.clone();
         });
         for (var i; i < bgchunk.length; i++)
         {
